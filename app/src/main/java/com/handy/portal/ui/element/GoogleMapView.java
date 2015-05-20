a357a3a85d48b379dcd1915bf28e23c0c30ffc56
cdavis-handy <cdavis@handy.com>
2015-05-20 11:24:47 -0400

    turn off prototype map, crashes

--- app/src/main/java/com/handy/portal/ui/element/GoogleMapView.java
+++ app/src/main/java/com/handy/portal/ui/element/GoogleMapView.java
@@ -1,11 +1,5 @@
 package com.handy.portal.ui.element;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
 import com.handy.portal.R;
 import com.handy.portal.core.booking.Booking;
 
@@ -29,27 +23,27 @@ public class GoogleMapView extends BookingDetailsView
         //initMapForBooking(booking);
     }
 
-    private void initMapForBooking(Booking booking)
-    {
-        MapView map = (MapView) parentViewGroup.findViewById(R.id.map);
-        GoogleMap googleMap = map.getMap();
-
-        float latitude = booking.getAddress().getLatitude();
-        float longitude = booking.getAddress().getLongitude();
-        LatLng target = new LatLng(latitude, longitude);
-
-        focusMap(googleMap, target, DEFAULT_ZOOM_LEVEL);
-    }
-
-    private void focusMap(GoogleMap googleMap, LatLng target, int zoomLevel)
-    {
-        CameraPosition targetCameraPosition = new CameraPosition.Builder().
-                target(target).
-                zoom(DEFAULT_ZOOM_LEVEL).
-                build();
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(targetCameraPosition);
-        googleMap.animateCamera(cameraUpdate);
-    }
+//    private void initMapForBooking(Booking booking)
+//    {
+//        MapView map = (MapView) parentViewGroup.findViewById(R.id.map);
+//        GoogleMap googleMap = map.getMap();
+//
+//        float latitude = booking.getAddress().getLatitude();
+//        float longitude = booking.getAddress().getLongitude();
+//        LatLng target = new LatLng(latitude, longitude);
+//
+//        focusMap(googleMap, target, DEFAULT_ZOOM_LEVEL);
+//    }
+//
+//    private void focusMap(GoogleMap googleMap, LatLng target, int zoomLevel)
+//    {
+//        CameraPosition targetCameraPosition = new CameraPosition.Builder().
+//                target(target).
+//                zoom(DEFAULT_ZOOM_LEVEL).
+//                build();
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(targetCameraPosition);
+//        googleMap.animateCamera(cameraUpdate);
+//    }
 
 
 
