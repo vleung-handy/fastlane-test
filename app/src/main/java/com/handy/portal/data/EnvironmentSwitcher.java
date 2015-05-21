6a2e353f143d46a4bb67b98d1e233e8b335ef85b
Roumwelle Sta Ines <rstaines@handy.com>
2015-05-21 11:13:30 -0400

    Remove prod as an option for environment toggles

--- app/src/main/java/com/handy/portal/data/EnvironmentSwitcher.java
+++ app/src/main/java/com/handy/portal/data/EnvironmentSwitcher.java
@@ -16,26 +16,12 @@ public class EnvironmentSwitcher
 
     public enum Environment
     {
-        P("Prod"), S("Staging"),
-        Q1, Q2, Q3, Q4, Q5, Q6, D1, D2, D3, D4, D5, D6, D7, D8, D9;
-
-        private String name;
-
-        Environment()
-        {
-            this.name = this.toString();
-        }
-
-        Environment(String name)
-        {
-            this.name = name;
-        }
+        S, Q1, Q2, Q3, Q4, Q5, Q6, D1, D2, D3, D4, D5, D6, D7, D8, D9;
 
         public String getName()
         {
-            return name;
+            return this.toString();
         }
-
 
         public String getPrefix()
         {
