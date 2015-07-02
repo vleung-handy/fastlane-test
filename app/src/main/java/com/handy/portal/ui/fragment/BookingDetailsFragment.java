c1920e95877088b3c1fbc6be64cb016261cb3de3
Roumwelle Sta Ines <rstaines@handy.com>
2015-07-02 18:20:08 -0400

    Render html for dialog warning text

--- app/src/main/java/com/handy/portal/ui/fragment/BookingDetailsFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/BookingDetailsFragment.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -456,7 +457,7 @@ public class BookingDetailsFragment extends InjectedFragment
         // set dialog message
         alertDialogBuilder
                 .setTitle(R.string.are_you_sure)
-                .setMessage(warning)
+                .setMessage(Html.fromHtml(warning))
                 .setPositiveButton(warningButtonsText.getPositiveStringId(), new DialogInterface.OnClickListener()
                         {
                             public void onClick(DialogInterface dialog, int id)
