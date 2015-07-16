7e1488e9467e185ef6cbe18cce693a5aa3616af9
Colin Davis <cdavis@handy.com>
2015-07-16 18:18:12 -0400

    native help tab

--- app/src/main/java/com/handy/portal/constant/MainViewTab.java
+++ app/src/main/java/com/handy/portal/constant/MainViewTab.java
@@ -1,5 +1,6 @@
 package com.handy.portal.constant;
 
+import com.handy.portal.help.HelpFragment;
 import com.handy.portal.ui.fragment.AvailableBookingsFragment;
 import com.handy.portal.ui.fragment.BookingDetailsFragment;
 import com.handy.portal.ui.fragment.PortalWebViewFragment;
@@ -10,8 +11,8 @@ public enum MainViewTab
     JOBS(null, AvailableBookingsFragment.class),
     SCHEDULE(null, ScheduledBookingsFragment.class),
     PROFILE(PortalWebViewFragment.Target.PROFILE, null),
-    //HELP(PortalWebViewFragment.Target.HELP, HelpFragment.class),
-    HELP(PortalWebViewFragment.Target.HELP, null),
+    HELP(PortalWebViewFragment.Target.HELP, HelpFragment.class),
+    //HELP(PortalWebViewFragment.Target.HELP, null),
     DETAILS(null, BookingDetailsFragment.class),;
 
     private PortalWebViewFragment.Target target;
