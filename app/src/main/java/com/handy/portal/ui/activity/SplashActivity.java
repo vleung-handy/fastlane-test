6227626fea2bd1912b2fe4b765be1b1f22bee437
Roumwelle Sta Ines <rstaines@handy.com>
2015-07-22 19:48:39 -0400

    On splash, extract user credentials from cookie manager and store it in prefs manager if possible

--- app/src/main/java/com/handy/portal/ui/activity/SplashActivity.java
+++ app/src/main/java/com/handy/portal/ui/activity/SplashActivity.java
@@ -2,13 +2,19 @@ package com.handy.portal.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 
 import com.crashlytics.android.Crashlytics;
 import com.handy.portal.R;
 import com.handy.portal.constant.PrefsKey;
 import com.handy.portal.event.HandyEvent;
 import com.handy.portal.manager.PrefsManager;
+import com.handy.portal.retrofit.HandyRetrofitEndpoint;
 import com.squareup.otto.Subscribe;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -20,6 +26,8 @@ public class SplashActivity extends BaseActivity
 
     @Inject
     PrefsManager prefsManager;
+    @Inject
+    HandyRetrofitEndpoint endpoint;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState)
@@ -27,17 +35,54 @@ public class SplashActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        String providerId = prefsManager.getString(PrefsKey.USER_CREDENTIALS_ID);
-        if (!providerId.isEmpty())
+        String providerId = prefsManager.getString(PrefsKey.USER_CREDENTIALS_ID, null);
+        if (providerId != null)
         {
             Crashlytics.setUserIdentifier(providerId);
+        }
+
+        String userCredentialsToken = prefsManager.getString(PrefsKey.USER_CREDENTIALS_TOKEN, null);
+        String userCredentialsTokenFromCookieManager = getUserCredentialsTokenFromCookieManager();
+        if (userCredentialsToken == null && userCredentialsTokenFromCookieManager != null)
+        {
+            prefsManager.setString(PrefsKey.USER_CREDENTIALS_TOKEN, userCredentialsTokenFromCookieManager);
+            if (providerId == null)
+            {
+                prefsManager.setString(PrefsKey.USER_CREDENTIALS_ID, "1"); // doesn't matter, we have a token
+            }
+
             checkForTerms();
         }
         else
         {
-            //TODO: Handle install referrers and deep links
             launchActivity(LoginActivity.class);
         }
+    }
+
+    private String getUserCredentialsTokenFromCookieManager()
+    {
+        CookieSyncManager.createInstance(this);
+        String allCookies = CookieManager.getInstance().getCookie(endpoint.getBaseUrl());
+        CookieSyncManager.getInstance().sync();
+
+        if (allCookies != null)
+        {
+            Pattern pattern = Pattern.compile(".*user_credentials=(.*?)(?:;|$).*");
+            Matcher matcher = pattern.matcher(allCookies);
+            if (matcher.matches())
+            {
+                return matcher.group(1);
+            }
+            else
+            {
+                Crashlytics.log("Token cannot be obtained from cookie manager");
+            }
+        }
+        else
+        {
+            Crashlytics.log("No cookies found in this device");
+        }
+        return null;
     }
 
     @Override
