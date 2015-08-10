7c04bffe7bd2d2f9cb03dab326f9423dad5aca18
Roumwelle Sta Ines <rstaines@handy.com>
2015-08-10 18:30:25 -0400

    Render html for the final tooltip

--- app/src/main/java/com/handy/portal/ui/fragment/TerminalOnboardingFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/TerminalOnboardingFragment.java
@@ -3,11 +3,13 @@ package com.handy.portal.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.handy.portal.R;
 import com.handy.portal.ui.activity.OnboardingActivity;
@@ -22,6 +24,8 @@ public class TerminalOnboardingFragment extends Fragment
     ImageView body;
     @InjectView(R.id.job_complete_image)
     ImageView jobCompleteImage;
+    @InjectView(R.id.bottom_tooltip)
+    TextView tooltip;
 
     private int bodyDrawableId;
 
@@ -43,6 +47,8 @@ public class TerminalOnboardingFragment extends Fragment
         body.setImageResource(bodyDrawableId);
         jobCompleteImage.setAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.fade_and_grow_in));
         jobCompleteImage.setVisibility(View.VISIBLE);
+
+        tooltip.setText(Html.fromHtml(getString(R.string.step_final)));
 
         return view;
     }
