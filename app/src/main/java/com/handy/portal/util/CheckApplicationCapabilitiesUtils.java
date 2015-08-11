1c338ff51cc76852c6622bef0f2104643f76a3cd
Victoria Leung <vleung@handybook.com>
2015-08-11 12:45:51 -0400

    NOR-191: line/space formatting

--- app/src/main/java/com/handy/portal/util/CheckApplicationCapabilitiesUtils.java
+++ app/src/main/java/com/handy/portal/util/CheckApplicationCapabilitiesUtils.java
@@ -20,8 +20,8 @@ public class CheckApplicationCapabilitiesUtils
         {
             int state = context.getPackageManager().getApplicationEnabledSetting(DOWNLOAD_MANAGER_PACKAGE_NAME);
             //is there a way to just check if there is a package that handles downloads?
-            return !(state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED ||
-                    state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER
+            return !(state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                    || state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER
                     || state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED_UNTIL_USED);
         } catch (Exception e)
         {
