bcbb291fac627da90a79a26e98ecb96834878ec0
Xi Wei <xwei@handy.com>
2015-09-11 10:03:44 -0400

    NOR-239 Minor fixes.

--- app/src/main/java/com/handy/portal/ui/fragment/PaymentsFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/PaymentsFragment.java
@@ -1,6 +1,5 @@
 package com.handy.portal.ui.fragment;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,16 +15,10 @@ public final class PaymentsFragment extends InjectedFragment
     public final View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                                    final Bundle savedInstanceState)
     {
-
         final View view = getActivity().getLayoutInflater()
                 .inflate(R.layout.fragment_payments, container, false);
 
         ButterKnife.inject(this, view);
-
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) //needed to workaround a bug in android 4.4 that cause webview artifacts to show.
-        {
-            view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
 
         return view;
     }
