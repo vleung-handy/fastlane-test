746f35ba41ce0d02076e1d5eddb8b0cf5eead00f
Xi Wei <xwei@handy.com>
2015-09-14 18:19:41 -0400

    NOR-262 Added menu icons. Enabled "Update banking information" to load the webview.

--- app/src/main/java/com/handy/portal/ui/fragment/PaymentsFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/PaymentsFragment.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.handy.portal.R;
+import com.handy.portal.retrofit.HandyRetrofitEndpoint;
+
+import javax.inject.Inject;
 
 import butterknife.ButterKnife;
 
-public final class PaymentsFragment extends ActionBarFragment
+public final class PaymentsFragment extends PortalWebViewFragment
 {
+    @Inject
+    HandyRetrofitEndpoint endpoint;
+
     @Override
     public void onResume()
     {
@@ -21,21 +29,25 @@ public final class PaymentsFragment extends ActionBarFragment
     }
 
     @Override
-    public final View onCreateView(final LayoutInflater inflater, final ViewGroup container,
-                                   final Bundle savedInstanceState)
-    {
-        final View view = getActivity().getLayoutInflater()
-                .inflate(R.layout.fragment_payments, container, false);
-
-        ButterKnife.inject(this, view);
-
-        return view;
-    }
-
-    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
     {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_payments, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_update_banking:
+                String url = endpoint.getBaseUrl() + "/portal/home?goto=profile";
+                Toast.makeText(getActivity(), url, Toast.LENGTH_SHORT).show();
+                openPortalUrl(url);
+                return true;
+            case R.id.action_email_verification:
+                Toast.makeText(getActivity(), "email verified", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
