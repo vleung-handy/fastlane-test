0b429bfb8bf1961e68d317d4d8f91cbf7a97fc87
Roumwelle Sta Ines <rstaines@handy.com>
2015-09-21 17:22:31 -0400

    Show booking details upon clicking a booking payment

--- app/src/main/java/com/handy/portal/ui/fragment/PaymentsDetailFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/PaymentsDetailFragment.java
@@ -9,14 +9,18 @@ import android.widget.ExpandableListView;
 import com.crashlytics.android.Crashlytics;
 import com.handy.portal.R;
 import com.handy.portal.constant.BundleKeys;
+import com.handy.portal.constant.MainViewTab;
+import com.handy.portal.event.HandyEvent;
+import com.handy.portal.model.Booking;
 import com.handy.portal.model.payments.NeoPaymentBatch;
-import com.handy.portal.ui.element.payments.PaymentDetailExpandableListView;
+import com.handy.portal.model.payments.Payment;
 import com.handy.portal.ui.element.payments.PaymentsDetailListHeaderView;
+import com.handy.portal.ui.element.payments.PaymentDetailExpandableListView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public final class PaymentsDetailFragment extends ActionBarFragment implements ExpandableListView.OnGroupClickListener
+public final class PaymentsDetailFragment extends ActionBarFragment implements ExpandableListView.OnGroupClickListener, ExpandableListView.OnChildClickListener
 {
     @InjectView(R.id.payments_detail_list_view)
     PaymentDetailExpandableListView paymentDetailExpandableListView; //using ExpandableListView because it is the only ListView that offers group view support
@@ -58,6 +62,7 @@ public final class PaymentsDetailFragment extends ActionBarFragment implements E
     {
         super.onViewCreated(view, savedInstanceState);
         paymentDetailExpandableListView.setOnGroupClickListener(this);
+        paymentDetailExpandableListView.setOnChildClickListener(this);
         paymentsDetailListHeaderView.updateDisplay(neoPaymentBatch);
         paymentDetailExpandableListView.updateData(neoPaymentBatch);
         for (int i = 0; i < neoPaymentBatch.getPaymentGroups().length; i++)
@@ -78,5 +83,27 @@ public final class PaymentsDetailFragment extends ActionBarFragment implements E
     public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id)
     {
         return true;
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id)
+    {
+        Payment payment = (Payment) parent.getExpandableListAdapter().getChild(groupPosition, childPosition);
+        String bookingId = payment.getBookingId();
+        String bookingType = payment.getBookingType() != null ? payment.getBookingType().toUpperCase() : Booking.BookingType.BOOKING.toString();
+        if (bookingId != null)
+        {
+            showBookingDetails(bookingId, bookingType);
+        }
+        return true;
+    }
+
+    private void showBookingDetails(String bookingId, String bookingType)
+    {
+        Bundle arguments = new Bundle();
+        arguments.putString(BundleKeys.BOOKING_ID, bookingId);
+        arguments.putString(BundleKeys.BOOKING_TYPE, bookingType);
+        HandyEvent.NavigateToTab event = new HandyEvent.NavigateToTab(MainViewTab.DETAILS, arguments);
+        bus.post(event);
     }
 }
