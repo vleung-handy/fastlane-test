b5d5e4976015d26622d916778d48333cf72394d3
Victoria Leung <vleung@handybook.com>
2015-09-22 16:52:43 -0400

    WIP: cleanup

--- app/src/main/java/com/handy/portal/model/payments/PaymentBatch.java
+++ app/src/main/java/com/handy/portal/model/payments/PaymentBatch.java
@@ -1,23 +1,10 @@
 package com.handy.portal.model.payments;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * Created by vleung on 9/16/15.
  */
 public class PaymentBatch implements Serializable
 {
-    public final Date getOldestDate()
-    {
-        if(this instanceof NeoPaymentBatch)
-        {
-            return ((NeoPaymentBatch) this).getStartDate();
-        }
-        else if(this instanceof LegacyPaymentBatch)
-        {
-            return ((LegacyPaymentBatch) this).getDate();
-        }
-        return null;
-    }
 }
