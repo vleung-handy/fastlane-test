66f5bbbae7bbcb01ac509ff760945c206a8ab6d2
Victoria Leung <vleung@handybook.com>
2015-09-22 16:52:43 -0400

    WIP: cleanup

--- app/src/main/java/com/handy/portal/manager/PaymentsManager.java
+++ app/src/main/java/com/handy/portal/manager/PaymentsManager.java
@@ -57,12 +57,6 @@ public class PaymentsManager
 
                 }
 
-
-                /**TEST ONLY**/
-
-//                paymentBatches.clearNeoPayments();
-
-                /**end test**/
                 bus.post(new PaymentEvents.ReceivePaymentBatchesSuccess(paymentBatches, startDate));
 
             }
