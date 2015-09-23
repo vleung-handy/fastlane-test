c2f66c2691c8f1625fe0fd7ad099f5c975843676
Victoria Leung <vleung@handybook.com>
2015-09-23 10:50:21 -0400

    hide payment group headers if payment group is empty

--- app/src/main/java/com/handy/portal/manager/PaymentsManager.java
+++ app/src/main/java/com/handy/portal/manager/PaymentsManager.java
@@ -3,11 +3,15 @@ package com.handy.portal.manager;
 import com.handy.portal.data.DataManager;
 import com.handy.portal.event.PaymentEvents;
 import com.handy.portal.model.payments.AnnualPaymentSummaries;
+import com.handy.portal.model.payments.NeoPaymentBatch;
 import com.handy.portal.model.payments.PaymentBatches;
+import com.handy.portal.model.payments.PaymentGroup;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -32,11 +36,27 @@ public class PaymentsManager
     {
         final Date startDate = event.startDate;
         Date endDate = event.endDate;
+        //assuming startDate is inclusive and endDate is exclusive
         dataManager.getPaymentBatches(startDate, endDate, new DataManager.Callback<PaymentBatches>()
         {
             @Override
             public void onSuccess(PaymentBatches paymentBatches)
             {
+                //for now, filter non-legacy payment batches to remove empty groups until server side changes are made
+                NeoPaymentBatch neoPaymentBatches[] = paymentBatches.getNeoPaymentBatches();
+                for(int i = 0; i<neoPaymentBatches.length; i++){
+                    PaymentGroup paymentGroups[] = neoPaymentBatches[i].getPaymentGroups();
+                    List<PaymentGroup> paymentGroupList = new LinkedList<PaymentGroup>();
+                    for(int j = 0; j<paymentGroups.length; j++)
+                    {
+                        if(paymentGroups[j].getPayments()!=null && paymentGroups[j].getPayments().length>0)
+                        {
+                            paymentGroupList.add(paymentGroups[j]);
+                        }
+                    }
+                    neoPaymentBatches[i].setPaymentGroups(paymentGroupList.toArray(new PaymentGroup[]{}));
+
+                }
                 bus.post(new PaymentEvents.ReceivePaymentBatchesSuccess(paymentBatches, startDate));
 
             }
