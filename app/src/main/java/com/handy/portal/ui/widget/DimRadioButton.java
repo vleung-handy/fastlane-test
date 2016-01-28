897d0e1396c787f5a1a66e682e66a58bfdff3313
Inder Dhir <idhir@handybook.com>
2016-01-28 15:18:43 -0500

    added extra constructors for the custom radio button

--- app/src/main/java/com/handy/portal/ui/widget/DimRadioButton.java
+++ app/src/main/java/com/handy/portal/ui/widget/DimRadioButton.java
@@ -1,6 +1,7 @@
 package com.handy.portal.ui.widget;
 
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.RadioButton;
@@ -8,9 +9,25 @@ import android.widget.RadioButton;
 public class DimRadioButton extends RadioButton
 {
 
+    public DimRadioButton(final Context context)
+    {
+        super(context);
+    }
+
     public DimRadioButton(final Context context, final AttributeSet attrs)
     {
         super(context, attrs);
+    }
+
+    public DimRadioButton(final Context context, final AttributeSet attrs, final int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public DimRadioButton(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
