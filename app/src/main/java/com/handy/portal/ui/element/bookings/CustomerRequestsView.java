51c390439112b09c53207663dd1ebfdfa18b23da
Xi Wei <xwei@handy.com>
2016-02-01 11:53:00 -0500

    Added title option for check list item.

--- app/src/main/java/com/handy/portal/ui/element/bookings/CustomerRequestsView.java
+++ app/src/main/java/com/handy/portal/ui/element/bookings/CustomerRequestsView.java
@@ -4,16 +4,15 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.handy.portal.R;
+import com.handy.portal.ui.widget.CheckListItemView;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -28,7 +27,7 @@ public class CustomerRequestsView extends FrameLayout
     @Bind(R.id.booking_details_job_instructions_checklist_entries_layout)
     LinearLayout mEntriesLayout;
 
-    private List<CheckBox> mCheckBoxEntries = new ArrayList<>();
+    private List<CheckListItemView> mCheckBoxEntries = new LinkedList<>();
 
     public CustomerRequestsView(final Context context, final String sectionTitle,
                                 @Nullable final Integer sectionIconId, final List<String> entries)
@@ -60,19 +59,18 @@ public class CustomerRequestsView extends FrameLayout
 
         for (String entry : entries)
         {
-            CheckBox blueCheckBox = (CheckBox) LayoutInflater.from(getContext())
-                    .inflate(R.layout.checkbox_blue_circle, mEntriesLayout, false);
-            blueCheckBox.setText(entry);
-            mEntriesLayout.addView(blueCheckBox);
-            mCheckBoxEntries.add(blueCheckBox);
+            CheckListItemView checkListItemView = new CheckListItemView(getContext());
+            checkListItemView.refreshDisplay(false, null, entry);
+            mEntriesLayout.addView(checkListItemView);
+            mCheckBoxEntries.add(checkListItemView);
         }
     }
 
     public boolean isChecked()
     {
-        for (CheckBox checkBox : mCheckBoxEntries)
+        for (CheckListItemView checkListItemView : mCheckBoxEntries)
         {
-            if (checkBox.isChecked()) { return true; }
+            if (checkListItemView.isChecked()) { return true; }
         }
         return false;
     }
---
+++ app/src/main/java/com/handy/portal/ui/widget/CheckListItemView.java
@@ -0,0 +1,82 @@
+package com.handy.portal.ui.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.handy.portal.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CheckListItemView extends FrameLayout
+{
+    @Bind(R.id.checklist_item_check_box)
+    CheckBox mCheckBox;
+    @Bind(R.id.checklist_item_title)
+    TextView mTitleTextView;
+    @Bind(R.id.checklist_item_description)
+    TextView mDescriptionTextView;
+
+
+    public CheckListItemView(final Context context)
+    {
+        super(context);
+        init();
+    }
+
+    public CheckListItemView(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    public CheckListItemView(final Context context, final AttributeSet attrs, final int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(21)
+    public CheckListItemView(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    public void refreshDisplay(boolean checked, @Nullable String title, @NonNull String description)
+    {
+        mCheckBox.setChecked(checked);
+        if (title != null && !title.isEmpty())
+        {
+            mTitleTextView.setVisibility(VISIBLE);
+            mTitleTextView.setText(title);
+        }
+        else
+        {
+            mTitleTextView.setVisibility(GONE);
+        }
+        mDescriptionTextView.setText(description);
+    }
+
+    public void setChecked(boolean checked)
+    {
+        mCheckBox.setChecked(checked);
+    }
+
+    public boolean isChecked()
+    {
+        return mCheckBox.isChecked();
+    }
+
+    private void init()
+    {
+        inflate(getContext(), R.layout.list_item_checkbox, this);
+        ButterKnife.bind(this);
+    }
+}
