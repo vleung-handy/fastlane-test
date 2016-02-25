9c4d0e5fa9fce880d34f235b9896cf4e6ada2159
Inder Dhir <idhir@handybook.com>
2016-02-25 12:42:34 -0500

    implemented the new and improved referral screen similar to consumer's

--- app/src/main/java/com/handy/portal/ui/fragment/ReferAFriendFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/ReferAFriendFragment.java
@@ -1,19 +1,63 @@
 package com.handy.portal.ui.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.handy.portal.R;
 import com.handy.portal.constant.MainViewTab;
+import com.handy.portal.event.HandyEvent;
+import com.handy.portal.event.LogEvent;
+import com.handy.portal.event.ProfileEvent;
+import com.handy.portal.manager.ProviderManager;
+import com.handy.portal.model.ProviderProfile;
+import com.handy.portal.model.ReferralInfo;
+import com.handy.portal.util.Utils;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
 
+import javax.inject.Inject;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ReferAFriendFragment extends ActionBarFragment
 {
+    @Inject
+    Bus mBus;
+    @Inject
+    ProviderManager mProviderManager;
+
+    @Bind(R.id.envelope)
+    View mEnvelope;
+    @Bind(R.id.envelope_shadow)
+    View mEnvelopeShadow;
+    @Bind(R.id.bling)
+    View mBling;
+
+    @Bind(R.id.profile_section_header_title_text)
+    TextView mTitleText;
+    @Bind(R.id.profile_section_header_subtitle_text)
+    TextView mSubtitleText;
+    @Bind(R.id.referral_code_text)
+    TextView mReferralCodeText;
+
+    @Bind(R.id.fetch_error_view)
+    ViewGroup fetchErrorLayout;
+    @Bind(R.id.fetch_error_text)
+    TextView fetchErrorText;
+
+    private View fragmentView;
+
+    private ProviderProfile mProviderProfile;
+    private ReferralInfo mReferralInfo;
 
     @Override
     protected MainViewTab getTab()
@@ -28,10 +72,15 @@ public class ReferAFriendFragment extends ActionBarFragment
             final Bundle savedInstanceState
     )
     {
-        final View view = inflater.inflate(R.layout.fragment_refer_a_friend, container, false);
-        ButterKnife.bind(this, view);
+        super.onCreateView(inflater, container, savedInstanceState);
+        if (fragmentView == null)
+        {
+            fragmentView = inflater.inflate(R.layout.fragment_refer_a_friend, container, false);
+        }
 
-        return view;
+        ButterKnife.bind(this, fragmentView);
+
+        return fragmentView;
     }
 
     @Override
@@ -39,5 +88,90 @@ public class ReferAFriendFragment extends ActionBarFragment
     {
         super.onResume();
         setActionBar(R.string.refer_a_friend, false);
+
+        populateInfo();
+    }
+
+    @OnClick(R.id.envelope)
+    public void onEnvelopeClicked()
+    {
+        mBling.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.sparkle_fade));
+    }
+
+    @OnClick(R.id.referral_code_layout)
+    public void createReferral()
+    {
+        mBus.post(new LogEvent.AddLogEvent(mEventLogFactory.createReferralSelectedLog()));
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, mReferralInfo.getReferralLink());
+        sendIntent.setType("text/plain");
+        Utils.safeLaunchIntent(Intent.createChooser(sendIntent, getContext().getString(R.string.share_with)), getContext());
+    }
+
+    @OnClick(R.id.try_again_button)
+    public void requestProviderProfile()
+    {
+        bus.post(new HandyEvent.SetLoadingOverlayVisibility(true));
+        bus.post(new ProfileEvent.RequestProviderProfile());
+    }
+
+    @Subscribe
+    public void onReceiveProviderProfileSuccess(ProfileEvent.ReceiveProviderProfileSuccess event)
+    {
+        bus.post(new HandyEvent.SetLoadingOverlayVisibility(false));
+        mProviderProfile = event.providerProfile;
+        mReferralInfo = mProviderProfile.getReferralInfo();
+
+        populateText();
+        startAnimations();
+    }
+
+    @Subscribe
+    public void onReceiveProviderProfileError(ProfileEvent.ReceiveProviderProfileError event)
+    {
+        bus.post(new HandyEvent.SetLoadingOverlayVisibility(false));
+        fetchErrorText.setText(R.string.error_loading_profile);
+        fetchErrorLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void populateInfo()
+    {
+        mProviderProfile = mProviderManager.getCachedProviderProfile();
+        if (mProviderProfile == null || mProviderProfile.getReferralInfo() == null)
+        {
+            requestProviderProfile();
+        }
+        else
+        {
+            mReferralInfo = mProviderProfile.getReferralInfo();
+            populateText();
+            startAnimations();
+        }
+    }
+
+    private void populateText()
+    {
+        mTitleText.setText(getContext().getString(R.string.earn_a_bonus, mReferralInfo.getBonusAmount()));
+        mSubtitleText.setText(R.string.refer_a_pro);
+        mReferralCodeText.setText(mReferralInfo.getReferralCode());
+    }
+
+    private void startAnimations()
+    {
+        mEnvelope.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.levitate));
+        mEnvelopeShadow.startAnimation(
+                AnimationUtils.loadAnimation(getActivity(), R.anim.expand_contract));
+        mBling.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if (isVisible())
+                {
+                    onEnvelopeClicked();
+                }
+            }
+        }, 1000);
     }
 }
