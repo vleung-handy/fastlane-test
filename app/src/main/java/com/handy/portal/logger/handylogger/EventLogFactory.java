ff378c7406f4923f66d5932992c54c0ed8cfc0c9
Xi Wei <xwei@handy.com>
2016-03-15 13:37:33 -0400

    Added event log for rolling ratings viewed

--- app/src/main/java/com/handy/portal/logger/handylogger/EventLogFactory.java
+++ app/src/main/java/com/handy/portal/logger/handylogger/EventLogFactory.java
@@ -345,11 +345,6 @@ public class EventLogFactory
         return new PerformanceLog.TierTappedLog();
     }
 
-    public EventLog createLifetimeRatingsLog()
-    {
-        return new PerformanceLog.LifetimeRatingsLog();
-    }
-
     // Help logs
     public EventLog createHelpContactFormSubmittedLog(String path, int helpNodeId, String helpNodeTitle)
     {
