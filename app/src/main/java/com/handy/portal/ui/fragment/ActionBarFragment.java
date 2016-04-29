c4ff6c3584b832cb54828e9b3044456261c8768a
Xi Wei <xwei@handy.com>
2016-04-29 14:17:01 -0400

    We cannot make a inner class serializable. Using bus event for tab selection instead.

--- app/src/main/java/com/handy/portal/ui/fragment/ActionBarFragment.java
+++ app/src/main/java/com/handy/portal/ui/fragment/ActionBarFragment.java
@@ -2,7 +2,6 @@ package com.handy.portal.ui.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -12,12 +11,10 @@ import android.view.View;
 
 import com.handy.portal.BuildConfig;
 import com.handy.portal.R;
-import com.handy.portal.constant.BundleKeys;
 import com.handy.portal.constant.MainViewTab;
 import com.handy.portal.core.EnvironmentModifier;
+import com.handy.portal.event.NavigationEvent;
 import com.handy.portal.util.UIUtils;
-
-import java.io.Serializable;
 
 import javax.inject.Inject;
 
@@ -25,27 +22,11 @@ public abstract class ActionBarFragment extends InjectedFragment
 {
     @Inject
     EnvironmentModifier environmentModifier;
-    private UpdateTabsCallback tabsCallback;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        Bundle args = getArguments();
-        if (args != null)
-        {
-            tabsCallback = (UpdateTabsCallback) args.getSerializable(BundleKeys.UPDATE_TAB_CALLBACK);
-        }
-
-        // If not given, create one that does nothing to avoid NullPointerException
-        if (tabsCallback == null)
-        {
-            tabsCallback = new UpdateTabsCallback()
-            {
-                @Override
-                public void updateTabs(MainViewTab tab) { }
-            };
-        }
     }
 
     @Override
@@ -58,7 +39,7 @@ public abstract class ActionBarFragment extends InjectedFragment
     public void onResume()
     {
         super.onResume();
-        tabsCallback.updateTabs(getTab());
+        bus.post(new NavigationEvent.SelectTab(getTab()));
     }
 
     @Override
@@ -182,9 +163,4 @@ public abstract class ActionBarFragment extends InjectedFragment
     }
 
     protected MainViewTab getTab() { return null; }
-
-    public interface UpdateTabsCallback extends Serializable
-    {
-        void updateTabs(@Nullable MainViewTab tab);
-    }
 }
