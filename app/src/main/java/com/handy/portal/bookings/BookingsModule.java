c6e6c318d674e400964bc951b038d2e1813dbd8b
Victoria Leung <vleung@handybook.com>
2016-05-03 16:21:15 -0400

    modularize the bookings, payments and location dagger dependencies

---
+++ app/src/main/java/com/handy/portal/bookings/BookingsModule.java
@@ -0,0 +1,49 @@
+package com.handy.portal.bookings;
+
+import com.handy.portal.bookings.ui.fragment.AvailableBookingsFragment;
+import com.handy.portal.bookings.ui.fragment.BookingDetailsWrapperFragment;
+import com.handy.portal.bookings.ui.fragment.BookingFragment;
+import com.handy.portal.bookings.ui.fragment.CancellationRequestFragment;
+import com.handy.portal.bookings.ui.fragment.ComplementaryBookingsFragment;
+import com.handy.portal.bookings.ui.fragment.InProgressBookingFragment;
+import com.handy.portal.bookings.ui.fragment.NearbyBookingsFragment;
+import com.handy.portal.bookings.ui.fragment.ScheduledBookingsFragment;
+import com.handy.portal.bookings.ui.fragment.SendReceiptCheckoutFragment;
+import com.handy.portal.bookings.ui.fragment.dialog.ConfirmBookingCancelDialogFragment;
+import com.handy.portal.bookings.ui.fragment.dialog.ConfirmBookingClaimDialogFragment;
+import com.handy.portal.bookings.ui.fragment.dialog.RateBookingDialogFragment;
+import com.handy.portal.data.DataManager;
+import com.squareup.otto.Bus;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module(
+        library = true,
+        complete = false,
+        injects = {
+                BookingDetailsWrapperFragment.class,
+                ScheduledBookingsFragment.class,
+                AvailableBookingsFragment.class,
+                ComplementaryBookingsFragment.class,
+                RateBookingDialogFragment.class,
+                NearbyBookingsFragment.class,
+                CancellationRequestFragment.class,
+                ConfirmBookingClaimDialogFragment.class,
+                ConfirmBookingCancelDialogFragment.class,
+                BookingFragment.class,
+                InProgressBookingFragment.class,
+                SendReceiptCheckoutFragment.class,
+        })
+public final class BookingsModule
+{
+    @Provides
+    @Singleton
+    final BookingManager provideBookingManager(final Bus bus,
+                                               final DataManager dataManager)
+    {
+        return new BookingManager(bus, dataManager);
+    }
+}
