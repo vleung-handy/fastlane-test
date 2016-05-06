76423008a8e6c0694edd1125520e0ab75eec0b61
Roumwelle Sta Ines <rstaines@handy.com>
2016-05-06 15:06:38 -0400

    Show supplies image and description

--- app/src/main/java/com/handy/portal/preactivation/PurchaseSuppliesFragment.java
+++ app/src/main/java/com/handy/portal/preactivation/PurchaseSuppliesFragment.java
@@ -1,13 +1,40 @@
 package com.handy.portal.preactivation;
 
+import android.os.Bundle;
+import android.view.View;
+
 import com.handy.portal.R;
+import com.handy.portal.ui.view.SimpleContentLayout;
+
+import butterknife.Bind;
 
 public class PurchaseSuppliesFragment extends PreActivationSetupStepFragment
 {
+    @Bind(R.id.cost_summary)
+    SimpleContentLayout mCostSummary;
+    @Bind(R.id.delivery_summary)
+    SimpleContentLayout mDeliverySummary;
+    @Bind(R.id.products_summary)
+    SimpleContentLayout mProductsSummary;
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        // FIXME: Pull from server
+        mCostSummary.setContent("What does the kit cost?",
+                "It costs $75, and it\u2019ll be billed to your card.");
+        mDeliverySummary.setContent("When will I get it?",
+                "It should arrive 1 to 3 business days after you\u2019ve been activated.");
+        mProductsSummary.setContent("What does it include?",
+                " • 2 All purpose cleaner\n • 2 Tub and tile cleaner\n" +
+                        " • Handy apron");
+    }
+
     @Override
     protected int getLayoutResId()
     {
-        return 0;
+        return R.layout.view_purchase_supplies;
     }
 
     @Override
@@ -49,6 +76,7 @@ public class PurchaseSuppliesFragment extends PreActivationSetupStepFragment
     @Override
     protected void onSecondaryButtonClicked()
     {
+        // FIXME: Show confirmation slide-up
         goToStep(null);
     }
 }
