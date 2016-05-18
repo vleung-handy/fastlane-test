b83ae319451290dae683c7fc3487d2eaed5e5264
Roumwelle Sta Ines <rstaines@handy.com>
2016-05-18 14:57:45 -0400

    Insert schedule builder into supplies flow

--- app/src/main/java/com/handy/portal/preactivation/PreActivationFlowActivity.java
+++ app/src/main/java/com/handy/portal/preactivation/PreActivationFlowActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import com.handy.portal.R;
 import com.handy.portal.constant.BundleKeys;
 import com.handy.portal.model.onboarding.OnboardingSuppliesInfo;
+import com.handy.portal.onboarding.ui.activity.ScheduleBuilderFragment;
 import com.handy.portal.ui.activity.BaseActivity;
 import com.handy.portal.ui.activity.MainActivity;
 
@@ -63,6 +64,6 @@ public class PreActivationFlowActivity extends BaseActivity
 
     private void goToFirstStep()
     {
-        next(PurchaseSuppliesFragment.newInstance(mOnboardingSuppliesInfo), false);
+        next(ScheduleBuilderFragment.newInstance(mOnboardingSuppliesInfo), false);
     }
 }
