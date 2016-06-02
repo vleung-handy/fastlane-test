5289f0b2ac7c4b875efc405bb952f9c3ba998cdc
Roumwelle Sta Ines <rstaines@handy.com>
2016-06-02 11:59:48 -0400

    Add @NonNull annotation

--- app/src/main/java/com/handy/portal/preactivation/OnboardingStatusFragment.java
+++ app/src/main/java/com/handy/portal/preactivation/OnboardingStatusFragment.java
@@ -3,6 +3,7 @@ package com.handy.portal.preactivation;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -120,7 +121,7 @@ public class OnboardingStatusFragment extends PreActivationFlowFragment
 
     }
 
-    private void addLinkTextView(final String text, final String url)
+    private void addLinkTextView(final String text, @NonNull final String url)
     {
         final TextView view = (TextView) LayoutInflater.from(getActivity())
                 .inflate(R.layout.view_link_text, mLinksContainer, false);
