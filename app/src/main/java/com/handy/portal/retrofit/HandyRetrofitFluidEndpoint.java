c0628e7b907c49dfe7e7927a42eabb1e8c622d9c
Roumwelle Sta Ines <rstaines@handy.com>
2016-06-24 16:13:46 -0400

    Support different domain formats for Q

--- app/src/main/java/com/handy/portal/retrofit/HandyRetrofitFluidEndpoint.java
+++ app/src/main/java/com/handy/portal/retrofit/HandyRetrofitFluidEndpoint.java
@@ -12,6 +12,9 @@ import java.util.Properties;
 
 public class HandyRetrofitFluidEndpoint extends HandyRetrofitEndpoint
 {
+    private static final String PROVISIONED_Q_PREFIX = "q";
+    private static final String DOMAIN_HBINTERNAL = "hbinternal";
+    private static final String DOMAIN_HANDY_INTERNAL = "handy-internal";
     private final EnvironmentModifier mEnvironmentModifier;
     private final Properties mProperties;
 
@@ -57,7 +60,24 @@ public class HandyRetrofitFluidEndpoint extends HandyRetrofitEndpoint
         final String environmentPrefix = mEnvironmentModifier.getEnvironmentPrefix();
         if (!TextUtils.isNullOrEmpty(environmentPrefix))
         {
-            return MessageFormat.format(url, environmentPrefix);
+            if (mEnvironmentModifier.getEnvironment() == EnvironmentModifier.Environment.Q)
+            {
+                // Q could be spun up through docker which will give it "handy-internal.com" domain
+                // or through normal Q provisioning which will give it "hbinternal.com" domain.
+                if (environmentPrefix.startsWith(PROVISIONED_Q_PREFIX))
+                {
+                    return MessageFormat.format(url, environmentPrefix, DOMAIN_HBINTERNAL);
+                }
+                else
+                {
+                    return MessageFormat.format(url, environmentPrefix, DOMAIN_HANDY_INTERNAL);
+                }
+
+            }
+            else
+            {
+                return MessageFormat.format(url, environmentPrefix);
+            }
         }
         else
         {
