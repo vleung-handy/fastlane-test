dd7d1fc4c30219a12d20fe0d35818bfb8185e462
Roumwelle Sta Ines <rstaines@handy.com>
2016-08-03 18:21:23 -0400

    Populate booking information is swap dialog

--- app/src/main/java/com/handy/portal/bookings/ui/fragment/dialog/SwapBookingClaimDialogFragment.java
+++ app/src/main/java/com/handy/portal/bookings/ui/fragment/dialog/SwapBookingClaimDialogFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.handy.portal.R;
 import com.handy.portal.bookings.model.Booking;
+import com.handy.portal.bookings.ui.element.AvailableBookingElementView;
 import com.handy.portal.constant.BundleKeys;
 import com.handy.portal.logger.handylogger.LogEvent;
 import com.handy.portal.logger.handylogger.model.AvailableJobsLog;
@@ -17,10 +18,14 @@ import org.greenrobot.eventbus.EventBus;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+
 public class SwapBookingClaimDialogFragment extends ConfirmBookingActionDialogFragment
 {
     @Inject
     EventBus mBus;
+    @BindView(R.id.conflicting_jobs)
+    ViewGroup mConflictingJobsContainer;
 
     public static final String FRAGMENT_TAG = SwapBookingClaimDialogFragment.class.getName();
 
@@ -38,6 +43,32 @@ public class SwapBookingClaimDialogFragment extends ConfirmBookingActionDialogFr
                                                    final ViewGroup container)
     {
         return inflater.inflate(R.layout.layout_confirm_booking_swap, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        initSwappableJob();
+        initClaimableJob();
+    }
+
+    private void initSwappableJob()
+    {
+        final AvailableBookingElementView bookingViewMediator = new AvailableBookingElementView();
+        bookingViewMediator.initView(getActivity(), mBooking, null, mConflictingJobsContainer);
+        final View bookingView = bookingViewMediator.getAssociatedView();
+        restyleBookingView(bookingView);
+        mConflictingJobsContainer.addView(bookingView);
+    }
+
+    private void initClaimableJob()
+    {
+        final AvailableBookingElementView bookingViewMediator = new AvailableBookingElementView();
+        bookingViewMediator.initView(getActivity(), mBooking, null, mConflictingJobsContainer);
+        final View bookingView = bookingViewMediator.getAssociatedView();
+        restyleBookingView(bookingView);
+        mConflictingJobsContainer.addView(bookingView);
     }
 
     @Override
@@ -65,5 +96,21 @@ public class SwapBookingClaimDialogFragment extends ConfirmBookingActionDialogFr
     protected String getConfirmButtonText()
     {
         return getString(R.string.confirm_switch);
+    }
+
+    private void restyleBookingView(final View bookingView)
+    {
+        final View serviceText = bookingView.findViewById(R.id.booking_entry_service_text);
+        if (serviceText != null)
+        {
+            serviceText.setVisibility(View.GONE);
+        }
+
+        final View leftStrip =
+                bookingView.findViewById(R.id.booking_list_entry_left_strip_indicator);
+        if (leftStrip != null)
+        {
+            leftStrip.setVisibility(View.GONE);
+        }
     }
 }
