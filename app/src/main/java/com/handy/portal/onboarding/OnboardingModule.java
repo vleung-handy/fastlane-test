05afb1582822cc87cc59071e9991834cc16da6a0
Inder Dhir <idhir@handybook.com>
2016-08-25 16:27:51 -0400

    added new supplies onboarding step

--- app/src/main/java/com/handy/portal/onboarding/OnboardingModule.java
+++ app/src/main/java/com/handy/portal/onboarding/OnboardingModule.java
@@ -3,6 +3,7 @@ package com.handy.portal.onboarding;
 import com.handy.portal.onboarding.ui.activity.OnboardingFlowActivity;
 import com.handy.portal.onboarding.ui.activity.OnboardingSubflowActivity;
 import com.handy.portal.onboarding.ui.fragment.DeclineSuppliesDialogFragment;
+import com.handy.portal.onboarding.ui.fragment.NewPurchaseSuppliesFragment;
 import com.handy.portal.onboarding.ui.fragment.OnboardingStatusFragment;
 import com.handy.portal.onboarding.ui.fragment.PurchaseSuppliesConfirmationFragment;
 import com.handy.portal.onboarding.ui.fragment.PurchaseSuppliesFragment;
@@ -25,6 +26,7 @@ import dagger.Module;
                 SchedulePreferencesFragment.class,
                 ScheduleConfirmationFragment.class,
                 OnboardingStatusFragment.class,
+                NewPurchaseSuppliesFragment.class,
         })
 public class OnboardingModule
 {
