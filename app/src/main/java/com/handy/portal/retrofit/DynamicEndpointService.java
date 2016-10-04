cfcf2e4c953e6f6bc255e78a08840a88574d0318
Roumwelle Sta Ines <rstaines@handy.com>
2016-10-04 19:42:31 -0400

    Retrofit service tweaks

--- app/src/main/java/com/handy/portal/retrofit/DynamicEndpointService.java
+++ app/src/main/java/com/handy/portal/retrofit/DynamicEndpointService.java
@@ -1,14 +1,12 @@
 package com.handy.portal.retrofit;
 
 import retrofit.http.Body;
-import retrofit.http.FormUrlEncoded;
 import retrofit.http.Headers;
 import retrofit.http.POST;
 import retrofit.mime.TypedFile;
 
 public interface DynamicEndpointService
 {
-    @FormUrlEncoded
     @Headers("Content-Type: image/jpeg")
     @POST("/")
     void uploadImage(@Body TypedFile file, HandyRetrofitCallback cb);
