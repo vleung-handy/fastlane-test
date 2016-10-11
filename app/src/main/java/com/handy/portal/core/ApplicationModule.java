3e992c94945bd0484b02fb3eb7eddfbe8fd52040
Sammy Ng <sng@handy.com>
2016-10-11 15:14:38 -0400

    NOR-1022 Updated to use the monolith endpoint instead of hitting the service directly

--- app/src/main/java/com/handy/portal/core/ApplicationModule.java
+++ app/src/main/java/com/handy/portal/core/ApplicationModule.java
@@ -55,7 +55,6 @@ import com.handy.portal.retrofit.DynamicEndpointService;
 import com.handy.portal.retrofit.HandyRetrofitEndpoint;
 import com.handy.portal.retrofit.HandyRetrofitFluidEndpoint;
 import com.handy.portal.retrofit.HandyRetrofitService;
-import com.handy.portal.retrofit.logevents.EventLogEndpoint;
 import com.handy.portal.retrofit.logevents.EventLogService;
 import com.handy.portal.retrofit.stripe.StripeRetrofitEndpoint;
 import com.handy.portal.retrofit.stripe.StripeRetrofitService;
@@ -303,30 +302,6 @@ public final class ApplicationModule
         return restAdapter.create(StripeRetrofitService.class);
     }
 
-    //log events
-    @Provides
-    @Singleton
-    final EventLogEndpoint provideLogEventsEndpoint()
-    {
-        return new EventLogEndpoint(context);
-    }
-
-    @Provides
-    @Singleton
-    final EventLogService provideLogEventsService(final EventLogEndpoint endpoint)
-    {
-        final OkHttpClient okHttpClient = new OkHttpClient();
-        okHttpClient.setReadTimeout(60, TimeUnit.SECONDS);
-
-        final RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(endpoint)
-                .setRequestInterceptor(new RequestInterceptor()
-                {
-                    @Override
-                    public void intercept(RequestFacade request) { }
-                }).setClient(new OkClient(okHttpClient)).build();
-        return restAdapter.create(EventLogService.class);
-    }
-
     @Provides
     @Singleton
     final DynamicEndpoint provideDynamicEndpoint()
@@ -370,12 +345,11 @@ public final class ApplicationModule
     final DataManager provideDataManager(final HandyRetrofitService service,
                                          final HandyRetrofitEndpoint endpoint,
                                          final StripeRetrofitService stripeService, //TODO: refactor and move somewhere else?
-                                         final EventLogService eventLogService,
                                          final DynamicEndpoint dynamicEndpoint,
                                          final DynamicEndpointService dynamicEndpointService
     )
     {
-        return new DataManager(service, endpoint, stripeService, eventLogService, dynamicEndpoint, dynamicEndpointService);
+        return new DataManager(service, endpoint, stripeService, dynamicEndpoint, dynamicEndpointService);
     }
 
     @Provides
