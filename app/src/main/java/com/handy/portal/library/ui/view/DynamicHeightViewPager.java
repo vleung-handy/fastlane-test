448ba3c2adc4299d35dfb35de33c14a32967bb51
Roumwelle Sta Ines <rstaines@handy.com>
2016-12-14 13:57:42 -0500

    Fix issue with ViewPager height being forced to match parent

---
+++ app/src/main/java/com/handy/portal/library/ui/view/DynamicHeightViewPager.java
@@ -0,0 +1,43 @@
+package com.handy.portal.library.ui.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+// Android isn't all that great. ViewPager does not support wrap_content for its layout_height
+// attribute. In order get that effect, use this class instead.
+public class DynamicHeightViewPager extends ViewPager
+{
+    public DynamicHeightViewPager(final Context context)
+    {
+        super(context);
+    }
+
+    public DynamicHeightViewPager(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++)
+        {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec,
+                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int childHeight = child.getMeasuredHeight();
+            if (childHeight > height)
+            {
+                height = childHeight;
+            }
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
