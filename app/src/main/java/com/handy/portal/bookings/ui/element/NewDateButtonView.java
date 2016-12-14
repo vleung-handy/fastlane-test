d905b52e8e624ca7b35c2318dd426ca12bee33c1
Roumwelle Sta Ines <rstaines@handy.com>
2016-12-13 19:06:50 -0500

    Support for different states in NewDateButtonView

--- app/src/main/java/com/handy/portal/bookings/ui/element/NewDateButtonView.java
+++ app/src/main/java/com/handy/portal/bookings/ui/element/NewDateButtonView.java
@@ -2,8 +2,8 @@ package com.handy.portal.bookings.ui.element;
 
 import android.content.Context;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,27 +13,44 @@ import com.handy.portal.library.util.DateTimeUtils;
 import java.util.Calendar;
 import java.util.Date;
 
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NewDateButtonView extends LinearLayout
 {
     @BindView(R.id.day_text)
-    TextView mDayText;
+    TextView mDayOfWeekText;
     @BindView(R.id.month_text)
     TextView mMonthText;
     @BindView(R.id.date_text)
-    TextView mDateText;
+    TextView mDayOfMonthText;
     @BindView(R.id.schedule_indicator)
-    View mScheduleIndicator;
+    ImageView mScheduleIndicator;
+    @BindColor(R.color.handy_darkened_blue)
+    int mDarkBlue;
+    @BindColor(R.color.handy_blue)
+    int mBlue;
+    @BindColor(R.color.black)
+    int mBlack;
+    @BindColor(R.color.white)
+    int mWhite;
+
+    private final Date mDate;
+    private final int mDayOfMonth;
+    private boolean mIsSelected = false;
 
     public NewDateButtonView(final Context context, final Date date)
     {
         super(context);
-        init(date);
+        mDate = date;
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        mDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        init();
     }
 
-    private void init(final Date date)
+    private void init()
     {
         inflate(getContext(), R.layout.element_date_button_new, this);
         final LinearLayout.LayoutParams layoutParams =
@@ -46,20 +63,38 @@ public class NewDateButtonView extends LinearLayout
 
         ButterKnife.bind(this);
 
-        final Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        final int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-        mDayText.setText(String.valueOf(DateTimeUtils.getDayOfWeek(date).charAt(0)));
-        mDateText.setText(String.valueOf(dayOfMonth));
-        if (dayOfMonth == 1)
+        mDayOfWeekText.setText(String.valueOf(DateTimeUtils.getDayOfWeek(mDate).charAt(0)));
+        mDayOfMonthText.setText(String.valueOf(mDayOfMonth));
+        final boolean isFirstOfTheMonth = mDayOfMonth == 1;
+        mMonthText.setVisibility(isFirstOfTheMonth ? VISIBLE : INVISIBLE);
+        if (isFirstOfTheMonth)
         {
-            mMonthText.setText(DateTimeUtils.getMonthShortName(date));
-            mMonthText.setVisibility(VISIBLE);
+            mMonthText.setText(DateTimeUtils.getMonthShortName(mDate));
+        }
+        refreshState();
+    }
+
+    private void refreshState()
+    {
+        if (mIsSelected)
+        {
+            mMonthText.setVisibility(INVISIBLE);
+            mDayOfMonthText.setTextColor(mWhite);
+            mScheduleIndicator.setImageResource(R.drawable.circle_white);
         }
         else
         {
-            mMonthText.setVisibility(INVISIBLE);
+            mMonthText.setVisibility(mDayOfMonth == 1 ? VISIBLE : INVISIBLE);
+            final boolean isToday = DateTimeUtils.daysBetween(mDate, new Date()) == 0;
+            mDayOfMonthText.setTextColor(isToday ? mDarkBlue : mBlack);
+            mScheduleIndicator.setImageResource(R.drawable.circle_handy_blue);
         }
+    }
+
+    public void setSelected(final boolean isSelected)
+    {
+        mIsSelected = isSelected;
+        refreshState();
     }
 
     public void setScheduleIndicator(final boolean isShown)
