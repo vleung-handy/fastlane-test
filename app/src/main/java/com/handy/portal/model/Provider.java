61798f20f04e7d4a9abcbaebac5a6395be4df06a
Roumwelle Sta Ines <rstaines@handy.com>
2016-12-20 12:35:06 -0500

    Remove unused code in Provider model

--- app/src/main/java/com/handy/portal/model/Provider.java
+++ app/src/main/java/com/handy/portal/model/Provider.java
@@ -1,7 +1,5 @@
 package com.handy.portal.model;
 
-import android.support.annotation.Nullable;
-
 import com.google.gson.annotations.SerializedName;
 import com.handy.portal.constant.Country;
 
@@ -19,32 +17,12 @@ public class Provider
     private String country;
     @SerializedName("block_cleaner")
     private boolean isBlockCleaner;
-    //TODO: reorganize
-    @SerializedName("payment_currency_code")
-    private String paymentCurrencyCode;
-    @SerializedName("recommended_payment_flow")
-    private String recommendedPaymentFlow;
-    @SerializedName("version_track")
-    private String mVersionTrack;
 
     public boolean isBlockCleaner()
     {
         return isBlockCleaner;
     }
 
-    public enum RecommendedPaymentFlow{
-        STRIPE_DEBIT("stripe_debit"), STRIPE("stripe");
-        private String value;
-        RecommendedPaymentFlow(String value)
-        {
-            this.value = value;
-        }
-
-        public String getValue()
-        {
-            return value;
-        }
-    }
     public String getId()
     {
         return id;
@@ -58,16 +36,6 @@ public class Provider
     public String getFirstName()
     {
         return firstName;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public String getAbbreviatedName()
-    {
-        return firstName + (lastName.isEmpty() ? "" : " " + lastName.charAt(0) + ".");
     }
 
     public String getFullName()
@@ -89,17 +57,4 @@ public class Provider
     {
         return Country.US.equalsIgnoreCase(getCountry());
     }
-
-    public String getPaymentCurrencyCode()
-    {
-        return paymentCurrencyCode;
-    }
-
-    public String getRecommendedPaymentFlow()
-    {
-        return recommendedPaymentFlow;
-    }
-
-    @Nullable
-    public String getVersionTrack() { return mVersionTrack; }
 }
