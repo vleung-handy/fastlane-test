e060bdc0ed6779e7c5be02cdd4a15731257f3c23
Roumwelle Sta Ines <rstaines@handy.com>
2017-05-30 16:03:41 -0400

    Support links in announcements

--- app/src/main/java/com/handy/portal/announcements/ui/AnnouncementView.java
+++ app/src/main/java/com/handy/portal/announcements/ui/AnnouncementView.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -64,7 +66,15 @@ public class AnnouncementView extends FrameLayout {
             mTitleText.setVisibility(VISIBLE);
             mTitleText.setText(announcement.getTitle());
         }
-        mBodyText.setText(announcement.getSubtitle());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            mBodyText.setText(Html.fromHtml(announcement.getSubtitle(), Html.FROM_HTML_MODE_LEGACY));
+        }
+        else {
+            mBodyText.setText(Html.fromHtml(announcement.getSubtitle()));
+        }
+        mBodyText.setMovementMethod(LinkMovementMethod.getInstance());
+
         mActionButton.setText(actionButtonText);
         loadHeaderImage(announcement.getImageUrl());
 
