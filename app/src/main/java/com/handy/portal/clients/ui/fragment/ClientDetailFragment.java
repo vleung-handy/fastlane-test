e0edb3945676e96f13e0994a97f2250ccbc4d028
Sammy Ng <sng@handy.com>
2017-07-25 15:21:49 -0400

    Added circle of location

--- app/src/main/java/com/handy/portal/clients/ui/fragment/ClientDetailFragment.java
+++ app/src/main/java/com/handy/portal/clients/ui/fragment/ClientDetailFragment.java
@@ -1,10 +1,10 @@
 package com.handy.portal.clients.ui.fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +17,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.handy.portal.R;
 import com.handy.portal.clients.model.Client;
@@ -164,11 +166,20 @@ public class ClientDetailFragment extends ActionBarFragment {
             public void onMapReady(GoogleMap mMap) {
                 mGoogleMap = mMap;
                 Address address = mClient.getAddress();
-
-                CameraUpdate center =
-                        CameraUpdateFactory.newLatLng(new LatLng(address.getLatitude(),
-                                address.getLongitude()));
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                CameraUpdate center = CameraUpdateFactory.newLatLng(latLng);
                 CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+                // In meters
+                CircleOptions circleOptions = new CircleOptions()
+                        .center(latLng)
+                        .radius(300)
+                        .strokeColor(getContext().getResources().getColor(R.color.light_gray_trans))
+                        .strokeWidth(5)
+                        .fillColor(getContext().getResources().getColor(R.color.handy_blue_trans_10));
+
+                // Get back the mutable Circle
+                Circle circle = mGoogleMap.addCircle(circleOptions);
+
 
                 mGoogleMap.moveCamera(center);
                 mGoogleMap.animateCamera(zoom);
